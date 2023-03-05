f0f18667b782a270483c25b5e687f2a7ba2bf618
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2023-03-05 18:29:23 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+/*exercice4
+var MavariableString = "toto";
+var MavariableNumerique = prompt("entrez un nombre", 100);
+
+if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <= 100){
+    alert("je suis le cas 1");
+}else{
+    alert("je suis le cas 2");
+}
+*/
+/*exercice5
+var toto = innerHeight
+
+
+if(innerHeight > 500){
+    alert("coucou");
+}else{
+    alert("haaaa");
+}
+*/
+
+
+var n = prompt("entrez un nombre");
+var f = 1;
+for(var i=1;i<=n;i++){
+    f = f * i;   
+   
+}
+alert("le nombre ="+f);
+
+ 
+
