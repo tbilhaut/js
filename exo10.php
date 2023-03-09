f6312a2d9c1f52986e9191dc2fd145654776c3d9
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2023-03-09 13:15:13 +0100

    Create exo10.php

---
+++ exo10.php
@@ -0,0 +1,39 @@
+<style>
+		.rouge {
+			background-color: red;
+			height: 100px;
+			width: 100px;
+		}
+
+		.vert {
+			background-color: green;
+			height: 100px;
+			width: 100px;
+
+		}
+        .Dives {
+			background-color: blue;
+			height: 100px;
+			width: 100px;
+		}
+	</style>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    
+</head>
+<body>
+    <div id="MaDiveNuM1">Je suis La Div Num 1</div>
+    
+    <div id="maDiv" class="rouge">Transmutation<p>(click)</div>
+    <div id="Methode3" class="Dives">Je suis la (passer la souris)</div> 
+  
+</body>
+<script src='exo10.js'></script>
+
+</html>
