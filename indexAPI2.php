c10b5ff9740a62ee5f16cdcde865164459952bb6
tbilhaut <113522624+tbilhaut@users.noreply.github.com>
2023-03-15 17:29:09 +0100

    Create indexAPI2.php

---
+++ indexAPI2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen'>
+    
+</head>
+<body>
+    <h1>EXO</h1>
+    <div onclick="start()">Je suis un bouton</div>
+    <div id="arenne"></div>
+    
+</body>
+<script src='API2.js'></script>
+<script langage="text/javascript"></script>
+</html>
